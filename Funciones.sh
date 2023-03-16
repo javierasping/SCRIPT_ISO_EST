0200850cc24c84dfff7168beef3f3d38bf5bfb43
PedroOP96 <123730632+PedroOP96@users.noreply.github.com>
2023-03-16 11:11:08 +0100

    Añadir función f_conexión

--- Funciones.sh
+++ Funciones.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
 #Fichero de funciones
+
+#comprobamos si tenemos conexión
+
+function f_conexion {
+         if ping -c 1 -q 8.8.8.8 > /dev/null; then
+         echo -e "Tiene conexión"
+         return 0
+         else
+         echo -e "No tiene conexión"
+         return 1
+         fi
+ }
