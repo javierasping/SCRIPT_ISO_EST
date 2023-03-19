2010de2e1df5c61c55170fbafc760bc9c89e05c5
javierasping <javierasping@gmail.com>
2023-03-19 11:20:49 +0100

    Añadida función discos y corrección

--- Funciones.sh
+++ Funciones.sh
@@ -1,30 +1,55 @@
+#!/bin/bash
 #Fichero de funciones
 
 #1. Función para comprobar que somos root.
+
+function f_somosroot {
+    if [ $UID -eq 0 ]; then
+        return 0
+    else
+        echo "Para ejecutar este script es necesario que seas superusuario"
+        return 1
+    fi
+}
 
 #2. Función para comprobar conexión a internet.
 
 function f_conexion {
          if ping -c 1 -q 8.8.8.8 > /dev/null; then
-         echo -e "Tiene conexión"
          return 0
          else
-         echo -e "No tiene conexión"
+         echo -e "Para ejecutar este script es necesario que disponga de conexion a internet"
          return 1
          fi
  }
 
 #3. Función para comprobar que sistema operativo estamos utilizando.
 
-function f_sistemaoperativo {
+function f_sistemaoperativo { #noelia
                   cat /etc/os-release | grep 'ID'
-              echo -e “El sistema $sistema operativo es:”
+              echo  'El sistema $sistema operativo es:'
 }
+
+
+
+function f_sistema_operativo_V2 {
+    SO=$(lsb_release -d | awk '{print $2, $3}')
+    if [[ $SO == "Debian GNU/Linux" ]]; then
+        echo "El sistema operativo es Debian"
+        return 2
+    elif [[ $SO == "Rocky Linux" ]]; then
+        echo "El sistema operativo es Rocky Linux"
+        return 3
+    else
+        echo "Sistema operativo desconocido"
+    fi
+}
+
 
 
 #4. Función para actualizar los repesitorios.
 
-function f_actualización {
+function f_actualización_repositorios_debian {
             sudo apt update -y
             if [ $? -eq 0 ]; then
             echo -e “Se han actualizado los repositorios”
@@ -79,18 +104,30 @@ function f_existegdisk {
         fi
 }
 
-function f_detectadiscosvacios {
-    for i in {b..z}
-    do
-        if sfdisk -d /dev/vd$i 2>&1 | grep -q "does not contain a recognized partition table"; then
-            echo "/dev/vd$i"
-        fi
-    done
-}
+
+
 
 #6. Función para instalar dichos paquetes si están el sistema.
 
 #7. Función para comprobar el numero de dispositivos libres hay y además que>
+function f_detectadiscosvacios {
+    local discos=()
+    for i in {b..z}; do
+        if sfdisk -d /dev/vd$i 2>&1 | grep -q "does not contain a recognized partition table"; then
+            discos+=("/dev/vd$i")
+        fi
+        if [[ ${#discos[@]} -eq 4 ]]; then
+            break
+        fi
+    done
+
+    if [[ ${#discos[@]} -ne 4 ]]; then
+        echo "Error: no se detectaron 4 discos vacíos" >&2
+        return 1
+    fi
+
+    echo "${discos[@]}"
+}
 
 #8. Función para partcionar un disco como en la práctica 5.
 
--- ProgramaPrincipal.sh
+++ ProgramaPrincipal.sh
@@ -3,7 +3,28 @@
 
 #Enlazar programa funcional con el de funciones
 . ./Funciones.sh
- 
-f_conexion 
+
+#1.Comprobamos si somsos root
+f_somosroot
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+
+#2.Comprobar si tenemos conexion a internet 
+f_conexion
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+#3. Función para comprobar que sistema operativo estamos utilizando.
+
+#f_sistemaoperativo Noelia --> No hace lo que se pide  corregido en f_sistema_operativo_V2
+f_sistema_operativo_V2
+
+
 
 f_detectadiscosvacios
+if [ $? -ne 0 ]; then
+    exit 1
+fi
